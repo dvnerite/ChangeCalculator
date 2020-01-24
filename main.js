b81b01d0587238853fd80a5376afb25464103c92
dvnerite <57559064+dvnerite@users.noreply.github.com>
2020-01-24 15:00:50 -0800

    first commit

--- main.js
+++ main.js
@@ -1,1 +1,52 @@
 // Write your JavaScript here
+
+console.log("whats good Dev, you got this!")
+
+// document.getElementById("calculateChange").onclick("calculateChange");
+
+
+function calculateChange(amountPaid, amountDue) {
+    // console.log("calculateChange");
+
+    var amountPaid = document.getElementById("amount-received").value;
+    var amountDue = document.getElementById("amount-due").value;
+    var centsDue = amountPaid - amountDue;
+    centsDue = centsDue * 100;
+
+
+    dollars = Math.floor(centsDue/100);
+    remainder = centsDue - dollars * 100;
+    //centsDue -= dollars * 100;
+    document.getElementById('dollars-output').innerText = dollars;
+        console.log(dollars);
+
+    quarters = Math.floor(remainder/25);
+    remainder = remainder - quarters * 25;
+    //centsDue -= quarters * 25;
+    document.getElementById('quarters-output').innerText = quarters;
+        console.log(quarters);
+
+
+    dimes = Math.floor(remainder/10);
+    remainder = remainder - dimes * 10;
+    //centsDue -= dimes * 10;
+    document.getElementById('dimes-output').innerText = dimes;
+        console.log(dimes);
+
+    nickels = Math.floor(remainder/5);
+    remainder = remainder - nickels * 5;
+    //centsDue -= dollars * 5;
+    document.getElementById('nickels-output').innerText = nickels;
+        console.log(nickels);
+
+    pennies = Math.round(remainder);
+    //centsDue -= pennies * 1;
+    document.getElementById('pennies-output').innerText = pennies;
+        console.log(pennies);
+}
+
+var button = document.getElementById("calculate-change");
+button.addEventListener("click", calculateChange);
+
+
+
